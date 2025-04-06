d1edc8618d40a9a51ae755ce91981903a4bd2d6a
Miri Yehezkel <miri.yehezkel@snyk.io>
2025-04-06 15:12:52 +0300

    sast failure

--- typeorm-db.js
+++ typeorm-db.js
@@ -3,6 +3,10 @@ var EntitySchema = typeorm.EntitySchema;
 
 const Users = require("./entity/Users")
 
+// trigger SAST
+const query = new URLSearchParams(window.location.search).get('query');
+document.getElementById('search_query').innerHTML = query;
+
 typeorm.createConnection({
   name: "mysql",
   type: "mysql",
