79626b574109efc34371861e5f8f4e262c627df4
Miri Yehezkel <miri.yehezkel@snyk.io>
2025-04-06 15:13:20 +0300

    undo

--- typeorm-db.js
+++ typeorm-db.js
@@ -3,10 +3,6 @@ var EntitySchema = typeorm.EntitySchema;
 
 const Users = require("./entity/Users")
 
-// trigger SAST
-const query = new URLSearchParams(window.location.search).get('query');
-document.getElementById('search_query').innerHTML = query;
-
 typeorm.createConnection({
   name: "mysql",
   type: "mysql",
